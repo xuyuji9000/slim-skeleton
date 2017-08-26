ae3f2d521cac8abb4c73fed03074908c263abede
Karl Xu <karl.yogi.xu@gmail.com>
2017-08-27 04:00:54 +0800

    update dependencies

--- config/dependencies.php
+++ config/dependencies.php
@@ -3,37 +3,6 @@
 // Dependency Injection Container
 $container = $app->getContainer();
 
-/**
- * View Template Configuration
- */
-// php view
-// $container['view'] = new \Slim\Views\PhpRenderer("../templates/");
-
-// twig
-$container['view'] = function($c)
-{
-    $view = new \Slim\Views\Twig('../templates/', [
-        'cache' => '../cache'
-    ]);
-
-    // Initiate and add Slim specific extension
-    $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
-    $view->addExtension(new Slim\Views\TwigExtension($c['router'], $basePath));
-    return $view;
-};
-
-
-/*
-$container['db'] = function($c) {
-    return DatabaseFactory::create($c['settings']['database']);
-};
- */
-
-// Session configuration
-$container['session'] = function($c) {
-    return new \SlimSession\Helper;
-};
-
 // Log configuration
 $container['logger'] = function($c) {
     $logger = new \Monolog\Logger('slim_logger');
--- config/middleware.php
+++ config/middleware.php
@@ -1,12 +1,5 @@
 <?php
-// Application Middleware
-
-// e.g: $app->add(new \Slim\Csrf\Guard);
-$app->add(new \Slim\Middleware\Session([
-    'name' => 'dummy_name',
-    'autorefresh' => true,
-    'lifetime' => '20 min',
-]));
+/* Application Middleware */
 
 // Cross Domain Access
 $app->add(function($request, $response, $next){
