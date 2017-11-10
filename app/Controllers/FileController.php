116eae207111c1cd048d4027c92167bd4411c483
Karl Xu <karl.yogi.xu@gmail.com>
2017-11-10 19:34:54 +0800

    app/controller -> app/Controller

--- app/Controllers/FileController.php
+++ app/Controllers/FileController.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\controllers;
+namespace App\Controllers;
 
 use User;
 
--- config/routes.php
+++ config/routes.php
@@ -3,4 +3,4 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-$app->post('/home', App\controllers\FileController::class.':home');
+$app->post('/home', App\Controllers\FileController::class.':home');
